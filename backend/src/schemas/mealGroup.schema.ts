452f4b402b9c6f52b561d3c89b4575037a9710eb
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:54:49 +0100

    add mealGroup schema

---
+++ backend/src/schemas/mealGroup.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Professional } from './professional.schema';
+import { Meal } from './meal.schema';
+
+export type MealGroupDocument = MealGroup & Document;
+
+@Schema()
+export class MealGroup {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Professional',
+    required: true,
+  })
+  author: Professional;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: false })
+  extra: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }],
+    required: true,
+    default: [],
+  })
+  meals: Meal[];
+}
+
+export const MealGroupSchema = SchemaFactory.createForClass(MealGroup);
