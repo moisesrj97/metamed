d66a9f43017bef7a6391c72e8c4d335b4453fc47
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:14:08 +0100

    add cross-env and modify scripts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
