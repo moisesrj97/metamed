9de97e32dce472fc7d940f2de4f4067eaf49e64b
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:53:48 +0100

    add chat schema

---
+++ backend/src/schemas/chat.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Professional } from './professional.schema';
+import { Patient } from './patient.schema';
+import { Message } from './message.schema';
+
+export type ChatDocument = Chat & Document;
+
+@Schema()
+export class Chat {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Professional',
+    required: true,
+  })
+  professional: Professional;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Patient',
+    required: true,
+  })
+  patient: Patient;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
+    required: true,
+    default: [],
+  })
+  messages: Message[];
+}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
