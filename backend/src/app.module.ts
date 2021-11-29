1e77f01fc13ee4cf72127bab442ffd282b28d528
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:11:55 +0100

    add basic mongoose connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri: string =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI
+    : process.env.MONGO_URI_TEST;
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [LoggerModule.forRoot(), MongooseModule.forRoot(uri)],
   controllers: [AppController],
   providers: [AppService],
 })
