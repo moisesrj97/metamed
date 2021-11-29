4da7e006e3a15877e57897ba49d050d8e8b7bcb9
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:54:35 +0100

    add exerciseGroup schema

---
+++ backend/src/schemas/exerciseGroup.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Professional } from './professional.schema';
+import { Exercise } from './exercise.schema';
+
+export type ExerciseGroupDocument = ExerciseGroup & Document;
+
+@Schema()
+export class ExerciseGroup {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Professional',
+    required: true,
+  })
+  author: Professional;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: false })
+  extra: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }],
+    required: true,
+    default: [],
+  })
+  exercises: Exercise[];
+}
+
+export const ExerciseGroupSchema = SchemaFactory.createForClass(ExerciseGroup);
