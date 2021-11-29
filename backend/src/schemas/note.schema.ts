e9948ad4ff97916724b4e41b375c0b8bc86c5329
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 12:54:26 +0100

    add note schema

---
+++ backend/src/schemas/note.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Professional } from './professional.schema';
+
+export type NoteDocument = Note & Document;
+
+@Schema()
+export class Note {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Professional',
+    required: true,
+  })
+  author: Professional;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: false })
+  description: string;
+}
+
+export const NoteSchema = SchemaFactory.createForClass(Note);
