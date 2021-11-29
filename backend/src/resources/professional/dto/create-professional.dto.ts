1ab04f3e33e5eb6abdc442c24eeaaa4459caea8b
moisesrj97 <moisesrj97@gmail.com>
2021-11-29 13:15:25 +0100

    create profesional entity and DTOs

--- backend/src/resources/professional/dto/create-professional.dto.ts
+++ backend/src/resources/professional/dto/create-professional.dto.ts
@@ -1,1 +1,20 @@
-export class CreateProfessionalDto {}
+export class CreateProfessionalDto {
+  name: string;
+  surname: string;
+  businessName: string;
+  email: string;
+  password: string;
+  constructor(
+    name: string,
+    surname: string,
+    businessName: string,
+    email: string,
+    password: string,
+  ) {
+    this.name = name;
+    this.surname = surname;
+    this.businessName = businessName;
+    this.email = email;
+    this.password = password;
+  }
+}
--- backend/src/resources/professional/entities/professional.entity.ts
+++ backend/src/resources/professional/entities/professional.entity.ts
@@ -1,1 +1,36 @@
-export class Professional {}
+import * as mongoose from 'mongoose';
+import { ExtraDataItem } from '../professional.schema';
+
+export type patientArrayElement = {
+  refData: mongoose.Types.ObjectId;
+  extraData: [ExtraDataItem];
+  chatRef: mongoose.Types.ObjectId;
+  exerciseGroups: [mongoose.Types.ObjectId];
+  mealGroups: [mongoose.Types.ObjectId];
+  notes: [mongoose.Types.ObjectId];
+};
+
+export class Professional {
+  role: string;
+  name: string;
+  surname: string;
+  businessName: string;
+  email: string;
+  password: string;
+  patients: patientArrayElement[];
+  constructor(
+    name: string,
+    surname: string,
+    businessName: string,
+    email: string,
+    password: string,
+  ) {
+    this.role = 'Professional';
+    this.name = name;
+    this.surname = surname;
+    this.businessName = businessName;
+    this.email = email;
+    this.password = password;
+    this.patients = [];
+  }
+}
