867c56fd7f4d7a7a3f6e0e3920b2608c43bcbf4e
moisesrj97 <moisesrj97@gmail.com>
2021-12-01 17:17:01 +0100

    add patient controller tests

--- backend/src/resources/patient/patient.controller.spec.ts
+++ backend/src/resources/patient/patient.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import CreatePatientDto from './dto/createPatient.dto';
+import UpdatePatientDto from './dto/updatePatient.dto';
 import { PatientController } from './patient.controller';
 import { PatientService } from './patient.service';
 
-describe('PatientController', () => {
+describe('Given the PatientController', () => {
   let controller: PatientController;
+  let service: PatientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +24,42 @@ describe('PatientController', () => {
     }).compile();
 
     controller = module.get<PatientController>(PatientController);
+    service = module.get<PatientService>(PatientService);
+  });
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When controller.create is executed', () => {
+    it('Service.create is called', () => {
+      controller.create(
+        new CreatePatientDto(),
+        '' as unknown as Express.Multer.File,
+      );
+
+      expect(service.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.findOne is executed', () => {
+    it('Service.findOne is called', () => {
+      controller.findOne('');
+
+      expect(service.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('Service.update is called', () => {
+      controller.update(
+        '',
+        new UpdatePatientDto(),
+        '' as unknown as Express.Multer.File,
+      );
+
+      expect(service.update).toHaveBeenCalled();
+    });
   });
 });
