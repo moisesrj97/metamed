16bf846799820fab617aafd71809e1b2e786e826
moisesrj97 <moisesrj97@gmail.com>
2021-12-01 19:47:20 +0100

    add login controller test

--- backend/src/resources/login/login.controller.spec.ts
+++ backend/src/resources/login/login.controller.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
 
-describe('LoginController', () => {
+describe('Given LoginController', () => {
   let controller: LoginController;
+  let service: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +23,39 @@ describe('LoginController', () => {
     }).compile();
 
     controller = module.get<LoginController>(LoginController);
+    service = module.get<LoginService>(LoginService);
+  });
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When controller.loginProfessionalWithToken is called', () => {
+    it('It should call service.loginProfessionalWithToken', () => {
+      controller.loginProfessionalWithToken('');
+      expect(service.loginProfessionalWithToken).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.loginProfessionalWithoutToken is called', () => {
+    it('It should call service.loginProfessionalWithoutToken', () => {
+      controller.loginProfessionalWithoutToken('', '');
+      expect(service.loginProfessionalWithoutToken).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.loginPatientWithToken is called', () => {
+    it('It should call service.loginPatientWithToken', () => {
+      controller.loginPatientWithToken('');
+      expect(service.loginPatientWithToken).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.loginPatientWithoutToken is called', () => {
+    it('It should call service.loginPatientWithoutToken', () => {
+      controller.loginPatientWithoutToken('', '');
+      expect(service.loginPatientWithoutToken).toHaveBeenCalled();
+    });
   });
 });
