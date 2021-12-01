53c61151f28fbf939f37a46ed6bef28e547e2cfd
moisesrj97 <moisesrj97@gmail.com>
2021-12-01 17:16:30 +0100

    add professional controller test

--- backend/src/resources/professional/professional.controller.spec.ts
+++ backend/src/resources/professional/professional.controller.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { S3ImageService } from '../../services/s3-image-service/s3-image-service.service';
+import CreateProfessionalDto from './dto/createProfessional.dto';
+import updateProfessionalDto from './dto/updateProfessional.dto';
 import { ProfessionalController } from './professional.controller';
 import { ProfessionalService } from './professional.service';
 
-describe('ProfessionalController', () => {
+describe('Given ProfessionalController...', () => {
   let controller: ProfessionalController;
+  let service: ProfessionalService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,9 +29,67 @@ describe('ProfessionalController', () => {
     }).compile();
 
     controller = module.get<ProfessionalController>(ProfessionalController);
+    service = module.get<ProfessionalService>(ProfessionalService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When it is instanciated...', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('When controller.create is executed', () => {
+    it('Service.create is called', () => {
+      controller.create(
+        new CreateProfessionalDto(),
+        '' as unknown as Express.Multer.File,
+      );
+
+      expect(service.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.findOne is executed', () => {
+    it('Service.findOne is called', () => {
+      controller.findOne('');
+
+      expect(service.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('Service.update is called', () => {
+      controller.update(
+        '',
+        new updateProfessionalDto(),
+        '' as unknown as Express.Multer.File,
+      );
+
+      expect(service.update).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.addPatient is executed', () => {
+    it('Service.addPatientToProfessional is called', () => {
+      controller.addPatient('', '');
+
+      expect(service.addPatientToProfessional).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.updatePatient is executed', () => {
+    it('Service.updatePatientFromProfessional is called', () => {
+      controller.updatePatient('', '', []);
+
+      expect(service.updatePatientFromProfessional).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.removePatient is executed', () => {
+    it('Service.removePatient is called', () => {
+      controller.removePatient('', '');
+
+      expect(service.removePatientFromProfessional).toHaveBeenCalled();
+    });
   });
 });
