8e17644f040507b4f79e94e07b87ef0edd9356f8
moisesrj97 <moisesrj97@gmail.com>
2021-12-01 09:18:35 +0100

    fix imports in patient/professional modules

--- backend/src/resources/patient/patient.module.ts
+++ backend/src/resources/patient/patient.module.ts
@@ -4,10 +4,16 @@ import { PatientController } from './patient.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Patient, PatientSchema } from './patient.schema';
 import { S3ImageService } from 'src/services/s3-image-service/s3-image-service.service';
+import { Chat, ChatSchema } from '../chat/chat.schema';
+import { Message, MessageSchema } from '../message/message.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),
+    MongooseModule.forFeature([
+      { name: Patient.name, schema: PatientSchema },
+      { name: Chat.name, schema: ChatSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
   ],
   controllers: [PatientController],
   providers: [PatientService, S3ImageService],
--- backend/src/resources/professional/professional.module.ts
+++ backend/src/resources/professional/professional.module.ts
@@ -6,6 +6,7 @@ import { Professional, ProfessionalSchema } from './professional.schema';
 import { S3ImageService } from 'src/services/s3-image-service/s3-image-service.service';
 import { Patient, PatientSchema } from '../patient/patient.schema';
 import { Chat, ChatSchema } from '../chat/chat.schema';
+import { Message, MessageSchema } from '../message/message.schema';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { Chat, ChatSchema } from '../chat/chat.schema';
       { name: Professional.name, schema: ProfessionalSchema },
       { name: Patient.name, schema: PatientSchema },
       { name: Chat.name, schema: ChatSchema },
+      { name: Message.name, schema: MessageSchema },
     ]),
   ],
   controllers: [ProfessionalController],
