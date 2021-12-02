b85eb3e3db994e312dff82a8bd2dc0ed590853f5
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 16:05:23 +0100

    add meal controller test

--- backend/src/resources/meal/meal.controller.spec.ts
+++ backend/src/resources/meal/meal.controller.spec.ts
@@ -1,20 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MealController } from './meal.controller';
 import { MealService } from './meal.service';
+import * as mongoose from 'mongoose';
 
-describe('MealController', () => {
+describe('Given MealController', () => {
   let controller: MealController;
+  let service: MealService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MealController],
-      providers: [MealService],
+      providers: [
+        {
+          provide: MealService,
+          useValue: {
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MealController>(MealController);
+    service = module.get<MealService>(MealService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('When controller.create is executed', () => {
+    it('Service.create is called', () => {
+      controller.create(
+        {
+          author: new mongoose.Types.ObjectId('f8f8f8f8f8f8'),
+          name: '',
+          amount: '',
+          mealGroupId: '',
+        },
+        '',
+      );
+
+      expect(service.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('Service.update is called', () => {
+      controller.update(
+        '',
+        {
+          name: '',
+          amount: '',
+        },
+        '',
+      );
+
+      expect(service.update).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.remove is executed', () => {
+    it('Service.remove is called', () => {
+      controller.remove('', '');
+
+      expect(service.remove).toHaveBeenCalled();
+    });
   });
 });
