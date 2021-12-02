141d25ec367efb31a88339b3cf570cc64fc3ae88
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 18:30:53 +0100

    add message controller test

--- backend/src/resources/message/message.controller.spec.ts
+++ backend/src/resources/message/message.controller.spec.ts
@@ -2,19 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 
-describe('MessageController', () => {
+describe('Given MessageController', () => {
   let controller: MessageController;
+  let service: MessageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MessageController],
-      providers: [MessageService],
+      providers: [
+        {
+          provide: MessageService,
+          useValue: {
+            update: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MessageController>(MessageController);
+    service = module.get<MessageService>(MessageService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('Service.update should be called', () => {
+      controller.update('', '');
+
+      expect(service.update).toHaveBeenCalled();
+    });
   });
 });
