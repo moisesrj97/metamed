2795bdd3e6389f4c86fe70321b25e2e39c25fbf8
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 15:53:39 +0100

    add meal service

--- backend/src/resources/meal/meal.service.ts
+++ backend/src/resources/meal/meal.service.ts
@@ -1,26 +1,103 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import validateJwt, { JwtInterface } from '../../helpers/validateJwt';
+import { MealGroup, MealGroupDocument } from '../meal-group/mealGroup.schema';
 import { CreateMealDto } from './dto/create-meal.dto';
 import { UpdateMealDto } from './dto/update-meal.dto';
+import { Meal, MealDocument } from './meal.schema';
 
 @Injectable()
 export class MealService {
-  create(createMealDto: CreateMealDto) {
-    return 'This action adds a new meal';
+  constructor(
+    @InjectModel(Meal.name)
+    private mealModel: Model<MealDocument>,
+    @InjectModel(MealGroup.name)
+    private mealGroupModel: Model<MealGroupDocument>,
+  ) {}
+
+  async create(token: string, createMealDto: CreateMealDto) {
+    let response: JwtInterface;
+    try {
+      response = validateJwt(token);
+    } catch (e) {
+      throw new Error('You are not authorized to perform this action');
+    }
+
+    if (response.role !== 'Professional') {
+      throw new Error('You are not authorized to perform this action');
+    }
+
+    const createdExercise = await this.mealModel.create({
+      author: response.id,
+      name: createMealDto.name,
+      amount: createMealDto.amount,
+    });
+
+    return await this.mealGroupModel.findByIdAndUpdate(
+      createMealDto.mealGroupId,
+      {
+        $push: {
+          exercises: createdExercise._id,
+        },
+      },
+      { new: true },
+    );
   }
 
-  findAll() {
-    return `This action returns all meal`;
+  async update(token: string, id: string, updateMealDto: UpdateMealDto) {
+    let response: JwtInterface;
+
+    try {
+      response = validateJwt(token);
+    } catch (e) {
+      throw new Error('You are not authorized to perform this action');
+    }
+
+    if (response.role !== 'Professional') {
+      throw new Error('You are not authorized to perform this action');
+    }
+
+    const meal = await this.mealModel.findById(id);
+    console.log(meal.author.toString(), response.id);
+
+    if (!meal || meal.author.toString() !== response.id) {
+      throw new Error('Exercise not found');
+    }
+
+    const { amount, name } = updateMealDto;
+
+    return await this.mealModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: { amount, name } },
+      { new: true },
+    );
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} meal`;
-  }
+  async remove(token: string, id: string) {
+    let response: JwtInterface;
 
-  update(id: number, updateMealDto: UpdateMealDto) {
-    return `This action updates a #${id} meal`;
-  }
+    try {
+      response = validateJwt(token);
+    } catch (e) {
+      throw new Error('You are not authorized to perform this action');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} meal`;
+    if (response.role !== 'Professional') {
+      throw new Error('You are not authorized to perform this action');
+    }
+
+    const meal = await this.mealModel.findById(id);
+
+    if (!meal || meal.author.toString() !== response.id) {
+      throw new Error('Meal not found');
+    }
+
+    await this.mealGroupModel.updateMany(
+      { meals: { $elemMatch: { $eq: id } } },
+      { $pull: { meals: id } },
+    );
+
+    return { message: 'Meal removed successfully from groups' };
   }
 }
