eabeae748d436745e85b7f4b63b8280eca75a4aa
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 18:21:06 +0100

    add chat controller and service tests

--- backend/src/resources/chat/chat.controller.spec.ts
+++ backend/src/resources/chat/chat.controller.spec.ts
@@ -2,19 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 
-describe('ChatController', () => {
+describe('Given ChatController', () => {
   let controller: ChatController;
+  let service: ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
-      providers: [ChatService],
+      providers: [
+        {
+          provide: ChatService,
+          useValue: {
+            update: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
+    service = module.get<ChatService>(ChatService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('Service.update should be called', () => {
+      controller.update('', { to: '', text: '' }, '');
+
+      expect(service.update).toHaveBeenCalled();
+    });
   });
 });
--- backend/src/resources/chat/chat.service.spec.ts
+++ backend/src/resources/chat/chat.service.spec.ts
@@ -1,18 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import * as mongoose from 'mongoose';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Message, MessageSchema } from '../message/message.schema';
+import { Chat, ChatSchema } from './chat.schema';
 
-describe('ChatService', () => {
+describe('Given ChatService', () => {
   let service: ChatService;
 
   beforeEach(async () => {
+    const mockRepository = {
+      create() {
+        return 'create model';
+      },
+      findByIdAndUpdate() {
+        return 'find model';
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [ChatService],
-    }).compile();
+      imports: [
+        MongooseModule.forFeature([
+          { name: Message.name, schema: MessageSchema },
+          { name: Chat.name, schema: ChatSchema },
+        ]),
+      ],
+    })
+      .overrideProvider(getModelToken('Chat'))
+      .useValue(mockRepository)
+      .overrideProvider(getModelToken('Message'))
+      .useValue(mockRepository)
+      .compile();
 
     service = module.get<ChatService>(ChatService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+  });
+
+  describe('When service.update is executed with valid token', () => {
+    it('It should return data without errors', async () => {
+      process.env.JWT_SECRET = 'test';
+
+      const response = await service.update(
+        'f8f8f8f8f8f8',
+        { to: 'f9f9f9f9f9f9', text: 'Hello test' },
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUHJvZmVzc2lvbmFsIiwiaWQiOiJmOGY4ZjhmOGY4ZjkiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJuYW1lIjoidGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.jYXlg2_VxJ3WpmNPywOlNgmQZNP0q6qA8qf7vNZKM0k',
+      );
+
+      expect(response).toBe('find model');
+    });
+  });
+
+  describe('When service.update is executed with invalid token', () => {
+    it('It should return data without errors', async () => {
+      process.env.JWT_SECRET = 'test';
+
+      try {
+        await service.update(
+          'f8f8f8f8f8f8',
+          { to: 'f9f9f9f9f9f9', text: 'Hello test' },
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUHJvZmVzc2lvbmFsIiwiaWQiOiJmOGY4ZjhmOGY4ZjkiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJuYW1lIjoidGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.jYXlg2_VxJ3WpmNPywOlNgmQZNP0q6qA8qf7vNZKM0k',
+        );
+      } catch (e) {
+        expect(e).toEqual(
+          Error('You are not authorized to perform this action'),
+        );
+      }
+    });
   });
 });
