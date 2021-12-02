da4a357a5ee29eb25e1662368e7d575f7ecdd96e
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 16:38:43 +0100

    add note controller

--- backend/src/resources/note/note.controller.ts
+++ backend/src/resources/note/note.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Headers,
+} from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { DeleteNoteDto } from './dto/delete-note.dto';
 
 @Controller('note')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Post()
-  create(@Body() createNoteDto: CreateNoteDto) {
-    return this.noteService.create(createNoteDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.noteService.findAll();
+  create(
+    @Body() createNoteDto: CreateNoteDto,
+    @Headers('Authorization') token: string,
+  ) {
+    return this.noteService.create(createNoteDto, token);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.noteService.findOne(+id);
+  getById(@Param('id') id: string, @Headers('Authorization') token: string) {
+    return this.noteService.getById(id, token);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.noteService.update(+id, updateNoteDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateNoteDto: UpdateNoteDto,
+    @Headers('Authorization') token: string,
+  ) {
+    return this.noteService.update(id, updateNoteDto, token);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.noteService.remove(+id);
+  remove(
+    @Param('id') id: string,
+    @Body() deleteNoteDto: DeleteNoteDto,
+    @Headers('Authorization') token: string,
+  ) {
+    return this.noteService.remove(id, deleteNoteDto, token);
   }
 }
