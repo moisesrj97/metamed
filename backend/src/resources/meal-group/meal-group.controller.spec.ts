f6b77c5855ac37fb6e1dd69c51e5c660486b5f29
moisesrj97 <moisesrj97@gmail.com>
2021-12-02 15:32:31 +0100

    add mealGroup controller test

--- backend/src/resources/meal-group/meal-group.controller.spec.ts
+++ backend/src/resources/meal-group/meal-group.controller.spec.ts
@@ -1,20 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as mongoose from 'mongoose';
 import { MealGroupController } from './meal-group.controller';
 import { MealGroupService } from './meal-group.service';
 
-describe('MealGroupController', () => {
+describe('Given MealGroupController', () => {
   let controller: MealGroupController;
+  let service: MealGroupService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MealGroupController],
-      providers: [MealGroupService],
+      providers: [
+        {
+          provide: MealGroupService,
+          useValue: {
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+            getById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MealGroupController>(MealGroupController);
+    service = module.get<MealGroupService>(MealGroupService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('When it is instanciated', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  });
+
+  describe('When controller.getById is executed', () => {
+    it('service.getById should be called', () => {
+      controller.getById('', '');
+      expect(service.getById).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.create is executed', () => {
+    it('service.create should be called', () => {
+      controller.create(
+        { patient: new mongoose.Types.ObjectId('f8f8f8f8f8f8'), name: '' },
+        '',
+      );
+      expect(service.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.update is executed', () => {
+    it('service.update should be called', () => {
+      controller.update('', { name: '', extra: '' }, '');
+      expect(service.update).toHaveBeenCalled();
+    });
+  });
+
+  describe('When controller.remove is executed', () => {
+    it('service.remove should be called', () => {
+      controller.remove('', { patientId: '' }, '');
+      expect(service.remove).toHaveBeenCalled();
+    });
   });
 });
