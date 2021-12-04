f0d46409e74ea371dc08f133bdd00b1862167221
moisesrj97 <moisesrj97@gmail.com>
2021-12-04 12:31:50 +0100

    add testing for auth service

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
   });
 
@@ -30,6 +29,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'frontend app is running!'
+    );
   });
 });
--- frontend/src/app/services/authentication/authentication.service.spec.ts
+++ frontend/src/app/services/authentication/authentication.service.spec.ts
@@ -1,16 +1,79 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AuthenticationService } from './authentication.service';
 
-describe('AuthenticationService', () => {
+const mockResponse: any = {
+  data: {
+    role: 'professional',
+    id: 'f8f8f8f8f8f8',
+  },
+};
+
+const mockToken: string = 'imamocktoken';
+
+describe('Given AuthenticationService', () => {
   let service: AuthenticationService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(AuthenticationService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('When it is instanciated', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('When loginWithToken is called', () => {
+    it('httpClient should be called', () => {
+      service.loginWithToken('token').subscribe((response: any) => {
+        expect(response).not.toBe(null);
+        expect(JSON.stringify(response)).toEqual(JSON.stringify(mockResponse));
+      });
+
+      const req = httpTestingController.expectOne(
+        'http://localhost:3000/login/token'
+      );
+
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('When loginWithoutToken is called', () => {
+    it('httpClient should be called', () => {
+      service
+        .loginWithoutToken('test', 'test', 'professional')
+        .subscribe((response: any) => {
+          expect(response).not.toBe(null);
+          expect(JSON.stringify(response)).toEqual(
+            JSON.stringify(mockResponse)
+          );
+        });
+
+      const req = httpTestingController.expectOne(
+        'http://localhost:3000/login/professional'
+      );
+
+      req.flush(mockResponse);
+
+      const req2 = httpTestingController.expectOne(
+        'http://localhost:3000/login/token'
+      );
+
+      req2.flush(mockResponse);
+    });
   });
 });
