866c7d7d764651dadbc5835450b67f21534b0cd0
moisesrj97 <moisesrj97@gmail.com>
2021-12-04 15:20:13 +0100

    add exerciseGroup service test

---
+++ frontend/src/app/services/exerciseGroup/exercise-group.service.spec.ts
@@ -0,0 +1,105 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { ExerciseGroupService } from './exercise-group.service';
+
+const mockResponse: any = {
+  data: {
+    role: 'professional',
+    id: 'f8f8f8f8f8f8',
+  },
+};
+
+describe('Given ExerciseGroupService', () => {
+  let service: ExerciseGroupService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(ExerciseGroupService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  describe('When it is instanciated', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('When addExerciseGroupToPatient is called', () => {
+    it('httpClient should be called', () => {
+      service
+        .addExerciseGroupToPatient('test', 'test', 'test')
+        .subscribe((response: any) => {
+          expect(response).not.toBe(null);
+          expect(JSON.stringify(response)).toEqual(
+            JSON.stringify(mockResponse)
+          );
+        });
+
+      const req = httpTestingController.expectOne({
+        method: 'POST',
+        url: 'http://localhost:3000/exercise-group',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('When updateExerciseGroupName is called', () => {
+    it('httpClient should be called', () => {
+      service
+        .updateExerciseGroupName('test', 'test', 'test')
+        .subscribe((response: any) => {
+          expect(response).not.toBe(null);
+          expect(JSON.stringify(response)).toEqual(
+            JSON.stringify(mockResponse)
+          );
+        });
+
+      const req = httpTestingController.expectOne({
+        method: 'PATCH',
+        url: 'http://localhost:3000/exercise-group/test',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('When getExerciseGroup is called', () => {
+    it('httpClient should be called', () => {
+      service.getExerciseGroup('test', 'test').subscribe((response: any) => {
+        expect(response).not.toBe(null);
+        expect(JSON.stringify(response)).toEqual(JSON.stringify(mockResponse));
+      });
+
+      const req = httpTestingController.expectOne({
+        method: 'GET',
+        url: 'http://localhost:3000/exercise-group/test',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('When deleteExerciseGroup is called', () => {
+    it('httpClient should be called', () => {
+      service.deleteExerciseGroup('test', 'test').subscribe((response: any) => {
+        expect(response).not.toBe(null);
+        expect(JSON.stringify(response)).toEqual(JSON.stringify(mockResponse));
+      });
+
+      const req = httpTestingController.expectOne({
+        method: 'DELETE',
+        url: 'http://localhost:3000/exercise-group/test',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+});
