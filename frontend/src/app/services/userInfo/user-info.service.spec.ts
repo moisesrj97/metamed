fc89083d1c23ffe5ad6308dd80de8f4ea9688467
moisesrj97 <moisesrj97@gmail.com>
2021-12-04 13:48:01 +0100

    add user-info service test

---
+++ frontend/src/app/services/userInfo/user-info.service.spec.ts
@@ -0,0 +1,91 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { UserInfoService } from './user-info.service';
+
+const mockResponse: any = {
+  data: {
+    role: 'professional',
+    id: 'f8f8f8f8f8f8',
+  },
+};
+
+describe('Given UserInfoService', () => {
+  let service: UserInfoService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(UserInfoService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  describe('When it is instanciated', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('When update is called on professional', () => {
+    it('httpClient should be called', () => {
+      service
+        .update(
+          '123',
+          'professional',
+          {
+            name: 'test',
+            surname: 'test',
+            profilePicture: 'test',
+          },
+          'helloiamatoken'
+        )
+        .subscribe((response: any) => {
+          expect(response).not.toBe(null);
+          expect(JSON.stringify(response)).toEqual(
+            JSON.stringify(mockResponse)
+          );
+        });
+
+      const req = httpTestingController.expectOne({
+        method: 'PATCH',
+        url: 'http://localhost:3000/professional/123',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+
+  describe('When update is called on patient', () => {
+    it('httpClient should be called', () => {
+      service
+        .update(
+          '123',
+          'patient',
+          {
+            name: 'test',
+            surname: 'test',
+            profilePicture: 'test',
+          },
+          'helloiamatoken'
+        )
+        .subscribe((response: any) => {
+          expect(response).not.toBe(null);
+          expect(JSON.stringify(response)).toEqual(
+            JSON.stringify(mockResponse)
+          );
+        });
+
+      const req = httpTestingController.expectOne({
+        method: 'PATCH',
+        url: 'http://localhost:3000/patient/123',
+      });
+
+      req.flush(mockResponse);
+    });
+  });
+});
