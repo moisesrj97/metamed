dbafdae68f23171133e6d6b1a891de93877f4cdd
moisesrj97 <moisesrj97@gmail.com>
2021-12-04 15:00:33 +0100

    add patients managment service test

---
+++ frontend/src/app/services/patientManagment/patient-managment.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExtraDataModel, UserStore } from 'src/app/models/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientManagmentService {
+  baseUrl = 'http://localhost:3000/professional';
+  constructor(private httpClient: HttpClient) {}
+
+  addPatientToList(
+    professionalId: string,
+    patientId: string,
+    token: string
+  ): Observable<UserStore> {
+    return this.httpClient.post(
+      `${this.baseUrl}/${professionalId}/patients`,
+      { patientId },
+      { headers: new HttpHeaders().set('Authorization', `Bearer ${token}`) }
+    ) as Observable<UserStore>;
+  }
+
+  updateExtraDataFromPatient(
+    professionalId: string,
+    patientId: string,
+    updatedData: ExtraDataModel[],
+    token: string
+  ): Observable<UserStore> {
+    return this.httpClient.patch(
+      `${this.baseUrl}/${professionalId}/patients/${patientId}`,
+      {
+        allExtraDataUpdated: updatedData,
+      },
+      { headers: new HttpHeaders().set('Authorization', `Bearer ${token}`) }
+    ) as Observable<UserStore>;
+  }
+
+  removePatientFromList(
+    professionalId: string,
+    patientId: string,
+    token: string
+  ): Observable<UserStore> {
+    return this.httpClient.delete(
+      `${this.baseUrl}/${professionalId}/patients/${patientId}`,
+      { headers: new HttpHeaders().set('Authorization', `Bearer ${token}`) }
+    ) as Observable<UserStore>;
+  }
+}
