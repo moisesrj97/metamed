73e12f92e4d6c2fd29e5517fd147618bacb07373
moisesrj97 <moisesrj97@gmail.com>
2021-12-05 18:15:54 +0100

    fix display behaviour when logged in in mobie
    it didn´t display the hamburguer menu

--- frontend/src/app/core/navbar/navbar.component.ts
+++ frontend/src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { UserStore } from 'src/app/models/interfaces';
 import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
@@ -9,7 +11,10 @@ import { TokenService } from 'src/app/services/token/token.service';
 export class NavbarComponent implements OnInit {
   loggedIn: boolean;
   @Output() toggleNav: EventEmitter<boolean>;
-  constructor(public tokenService: TokenService) {
+  constructor(
+    public tokenService: TokenService,
+    private store: Store<{ user: UserStore }>
+  ) {
     this.loggedIn = false;
     this.toggleNav = new EventEmitter<boolean>();
   }
@@ -19,8 +24,14 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.tokenService.getTokenFromLocalStorage()) {
-      this.loggedIn = true;
-    }
+    this.store
+      .select((state) => state.user._id)
+      .subscribe((id) => {
+        if (id !== '') {
+          this.loggedIn = true;
+        } else {
+          this.loggedIn = false;
+        }
+      });
   }
 }
