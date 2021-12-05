04cb4efb5704c89a494f01d17c93a2304c3f9ea7
moisesrj97 <moisesrj97@gmail.com>
2021-12-05 13:42:09 +0100

    export routes from app-routing-module for tests

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteAuthLazyGuard } from './guards/route-auth-lazy.guard';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
