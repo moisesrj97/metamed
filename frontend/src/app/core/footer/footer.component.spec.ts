46bfe3587a00a82790ab12ebf758627e22233f81
moisesrj97 <moisesrj97@gmail.com>
2021-12-06 09:05:54 +0100

    add footer tests

--- frontend/src/app/core/footer/footer.component.spec.ts
+++ frontend/src/app/core/footer/footer.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let store: MockStore;
+  let initialState = { user: { _id: '' } };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      providers: [provideMockStore()],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
   });
 
@@ -22,5 +24,19 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('With no valid _id in the store', () => {
+    it('Should set loggedIn to false', () => {
+      expect(component.isLoggedIn).toBeFalse();
+    });
+  });
+
+  describe('With no valid _id in the store', () => {
+    it('Should set loggedIn to false', () => {
+      TestBed.inject(MockStore).setState({ user: { _id: '123' } });
+
+      expect(component.isLoggedIn).toBeTrue();
+    });
   });
 });
