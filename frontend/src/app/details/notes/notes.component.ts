2fb83ba7235d6e59143a17e977c4d54e7f5ec5cb
moisesrj97 <moisesrj97@gmail.com>
2021-12-07 17:16:04 +0100

    add addNote to notes component

--- frontend/src/app/details/notes/notes.component.ts
+++ frontend/src/app/details/notes/notes.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { UserStore, PatientModel, NoteModel } from 'src/app/models/interfaces';
+import {
+  UserStore,
+  PatientModel,
+  NoteModel,
+  RefDataModel,
+} from 'src/app/models/interfaces';
 import { MealGroupService } from 'src/app/services/mealGroup/meal-group.service';
 import { NoteService } from 'src/app/services/note/note.service';
+import { addNote } from 'src/app/services/store/actions/note.actions';
 import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
@@ -15,6 +21,7 @@ export class NotesComponent implements OnInit {
   id!: string;
   data: string[] = [];
   fetchedData: NoteModel[] = [];
+  input: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -42,5 +49,33 @@ export class NotesComponent implements OnInit {
           });
         });
       });
+  }
+
+  addGroup() {
+    const token = this.tokenService.getTokenFromLocalStorage() as string;
+
+    if (this.input.length > 0) {
+      this.noteService
+        .addNoteToPatient(this.id, this.input, token)
+        .subscribe((data) => {
+          const newGroupId = data.patients
+            ?.find(
+              (patient) =>
+                patient.refData === (this.id as unknown as RefDataModel)
+            )
+            ?.notes.find((group) => ![...this.data].includes(group)) as string;
+
+          this.fetchedData = [];
+
+          this.store.dispatch(
+            addNote({
+              noteId: newGroupId,
+              patientId: this.id,
+            })
+          );
+        });
+    }
+
+    this.input = '';
   }
 }
