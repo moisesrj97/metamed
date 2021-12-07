eae931f47c91cc5a48d17030177f64458bf4a2e3
moisesrj97 <moisesrj97@gmail.com>
2021-12-07 08:40:29 +0100

    add modal component

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -5,17 +5,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { CardComponent } from './card/card.component';
 import { SearchComponent } from './search/search.component';
-
+import { FormsModule } from '@angular/forms';
+import { AddModalComponent } from './add-modal/add-modal.component';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    CardComponent,
-    SearchComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent, CardComponent, SearchComponent, AddModalComponent],
+  imports: [CommonModule, DashboardRoutingModule, FormsModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
