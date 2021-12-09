2093e33137b01a6c5a09e1edf870c650114b513e
moisesrj97 <moisesrj97@gmail.com>
2021-12-09 09:26:39 +0100

    add inbox template, style and basic funct

--- frontend/src/app/inbox/inbox.component.ts
+++ frontend/src/app/inbox/inbox.component.ts
@@ -1,10 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  PatientModel,
+  ProfessionalModel,
+  UserStore,
+} from '../models/interfaces';
 
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.scss'],
 })
-export class InboxComponent {
-  constructor() {}
+export class InboxComponent implements OnInit {
+  data!: PatientModel[] | ProfessionalModel[] | undefined;
+  userId!: string;
+  constructor(private store: Store<{ user: UserStore }>) {}
+
+  ngOnInit(): void {
+    this.store
+      .select((state) => state.user.patients)
+      .subscribe((data) => {
+        this.data = data;
+      });
+
+    this.store
+      .select((state) => state.user._id)
+      .subscribe((id) => {
+        this.userId = id;
+      });
+  }
 }
