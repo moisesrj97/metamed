ab28d3a7c5d593d9493413a4e4baa303b242fa70
moisesrj97 <moisesrj97@gmail.com>
2021-12-09 17:35:12 +0100

    pwa config

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { userReducer } from './services/store/user.reducer';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +23,12 @@ import { CoreModule } from './core/core.module';
     }),
     HttpClientModule,
     CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
