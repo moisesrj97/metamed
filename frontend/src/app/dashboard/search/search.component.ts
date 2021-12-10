c106e21506a9386bc094bb007e87bfff8065e496
moisesrj97 <moisesrj97@gmail.com>
2021-12-10 10:21:31 +0100

    add dashboard module darkMode

--- frontend/src/app/dashboard/search/search.component.ts
+++ frontend/src/app/dashboard/search/search.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   @Output() search!: EventEmitter<string>;
+  darkMode!: boolean;
 
-  constructor() {
+  constructor(public store: Store<{ darkMode: { darkMode: boolean } }>) {
     this.search = new EventEmitter<string>();
+  }
+
+  ngOnInit(): void {
+    this.store.select('darkMode').subscribe((state) => {
+      this.darkMode = state.darkMode;
+    });
   }
 
   emitSearch(event: Event) {
